715495074db9ed284269bb5cfc3d83e960e01e49
wilmoscote <wilfredmoscote@gmail.com>
2024-07-08 12:40:24 -0500

    PetProfile modal

--- src/pages/auth/auth1/login.js
+++ src/pages/auth/auth1/login.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 // material-ui
-import { Grid, Stack, Typography } from '@mui/material';
+import { Dialog, Grid, Stack, Typography } from '@mui/material';
 
 // project-imports
 import Logo from 'components/logo';
@@ -16,17 +16,60 @@ import IgsLogo from 'assets/images/auth/igsLogo.svg';
 import imgFacebook from 'assets/images/auth/facebook.svg';
 import imgTwitter from 'assets/images/auth/twitter.svg';
 import imgGoogle from 'assets/images/auth/google.svg';
+import PetProfile from 'components/PetProfile';
+import { useState } from 'react';
+import { PopupTransition } from 'components/@extended/Transitions';
 
 // ================================|| LOGIN ||================================ //
 
 const Login = () => {
   const { isLoggedIn } = useAuth();
 
+  const [open, setOpen] = useState(true);
+  const data = JSON.stringify({
+    "id": 2,
+    "uuid": "01907a32-290a-71d7-b58d-3a3a8334054c",
+    "name": "V\u00e9nus",
+    "img_profile": "https://apiveterinary.3fixesdev.com/storage/images/pet/profile/6685bbedc80af.webp",
+    "specie_id": 2,
+    "breed_id": 9,
+    "user_id": 3,
+    "gender": "female",
+    "birthday_date": "2018-09-11",
+    "deleted_at": null,
+    "created_at": "2024-07-03T20:03:46.000000Z",
+    "updated_at": "2024-07-03T21:00:29.000000Z",
+    "specie": {
+      "id": 2,
+      "name": "Perro",
+      "uuid": "0190797f-6809-71c0-a233-70cfdec4a054",
+      "name_alias": "perro",
+      "deleted_at": null,
+      "created_at": "2024-07-03T16:48:31.000000Z",
+      "updated_at": "2024-07-03T16:48:31.000000Z"
+    },
+    "breed": {
+      "id": 9,
+      "name": "Beagle",
+      "uuid": "0190797f-680e-70a1-a639-ba9607c61a72",
+      "name_alias": "beagle",
+      "specie_id": 2,
+      "deleted_at": null,
+      "created_at": "2024-07-03T16:48:31.000000Z",
+      "updated_at": "2024-07-03T16:48:31.000000Z"
+    }
+  })
+
+  const [pet, setPet] = useState(JSON.parse(data));
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <AuthWrapper>
       <Grid container spacing={3}>
         <Grid item xs={12} sx={{ textAlign: 'center' }}>
-        <img src={IgsLogo} alt="IGS Logo" width={60} style={{ marginBottom: "-10px" }} />
+          <img src={IgsLogo} alt="IGS Logo" width={60} style={{ marginBottom: "-10px" }} />
         </Grid>
         {/* <Grid item xs={12}>
           <Grid container spacing={1}>
@@ -70,6 +113,17 @@ const Login = () => {
           <AuthLoginClinic forgot="/auth/forgot-password" />
         </Grid>
       </Grid>
+
+      <Dialog
+        maxWidth="sm"
+        fullWidth
+        TransitionComponent={PopupTransition}
+        onClose={handleClose}
+        open={open}
+        sx={{ '& .MuiDialog-paper': { p: 0 } }}
+      >
+        <PetProfile pet={pet} onCancel={handleClose} />
+      </Dialog>
     </AuthWrapper>
   );
 };
